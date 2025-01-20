3a5e98faa803efea343349bbef363d730975ce22
Maroua Ezzaki Kadmiri <maroua.ezzaki@bootcamp-upgrade.com>
2025-01-20 17:46:26 +0100

    terminado ejercicios JS y JSON

--- Snorlax.js
+++ Snorlax.js
@@ -1,5 +1,6 @@
 // Importar el módulo readFileSync
 import { readFileSync } from 'fs'
+import { join } from 'path'
 
 // Leer el archivo JSON
 // TO-DO - Agregar la ruta relativa desde la carpeta del proyecto
@@ -53,39 +54,104 @@ for (let i = 0; i < data.abilities.length; i++) {
 // tres subarrays, formados por el nombre y la URL
 // Borra la propiedad abilties y vuelvela a crear dinamicamente
 
-console.log(data.abilities[0].ability)
+// console.log(data.abilities[0].ability)
+// console.log(Object.values(data.abilities[0].ability))
+// console.log(Object.values(data.abilities[1].ability))
+// console.log(Object.values(data.abilities[2].ability))
 
-console.log(Object.values(data.abilities[0].ability))
-console.log(Object.values(data.abilities[1].ability))
-console.log(Object.values(data.abilities[2].ability))
+const abilities = []
 
-let abilities = []
-
-abilities.push(Object.values(Object.values(data.abilities[0].ability))
-abilities.push(Object.values(Object.values(data.abilities[1].ability))
-abilities.push(Object.values(Object.values(data.abilities[2].ability))
+abilities.push(Object.values(data.abilities[0].ability))
+abilities.push(Object.values(data.abilities[1].ability))
+abilities.push(Object.values(data.abilities[2].ability))
 
 info.abilities = abilities
-console.log(info)
+// console.log(info.abilities)
+
 // 5. Usa el operado spread (...) para agregar al array vacio de game_indices de info
 // los datos contenidos en game_indices del json
+
+info.game_indices = [...data.game_indices]
+// console.log(info.game_indices)
 
 // 6. Modifica con un bucle cada una de las posiciones del array game_indices de info
 // para que solo contena un objeto formado ppr game_index y name
 
+for (let i = 0; i < info.game_indices.length; i++) {
+  info.game_indices[i] = { game_index: info.game_indices[i].game_index, name: info.game_indices[i].version.name }
+}
+// console.log(info)
 // 7. Dentro del JSON, accede a los nombres de los items de held_items.
 // Crea un array con solo los nombres y unelos para crear una string separado por <->
 // Guardalo dentro del info -> held_items, sobreescribiendo el array vacio
 
+const items = []
+items.push(data.held_items[0].item.name)
+items.push(data.held_items[1].item.name)
+// console.log(data.held_items[0].item.name)
+// console.log(items)
+
+const itemsUnidos = items.join('<->')
+console.log(itemsUnidos)
+
+info.held_items = itemsUnidos
+// console.log(info)
 // 8. De la clave moves del JSON, crea un array que contenga todos los nombres
 // de los movimientos. Tras ello ordenalos alfabeticamente y ingresa en el array vacio
 // de moves dentro los primeros 10 movimientos
+// hay que ir deslogando y arte por parte, si es propiedad con punto y si es array con []
+// console.log(data.moves[0].move.name)
+
+const movimientos = []
+
+for (let i = 0; i < data.moves.length; i++) {
+  movimientos.push(data.moves[i].move.name)
+}
+
+movimientos.sort()
+// console.log(movimientos)
+
+info.moves = [...movimientos.slice(0, 10)]
+
+// console.log(info)
 
 // 9. Crea un objeto compuesto de las claves hp, attack, defense,
 // specialAttack, specialDefense y speed. Ingresa los valores que se indican en JSON
 // referente a estos nombres. Tras ello ingresalo en info -> stats sustituyendo al array
 // vacio
+const stats = {
+
+  hp: data.stats[0].base_stat,
+  attack: data.stats[1].base_stat,
+  defense: data.stats[2].base_stat,
+  specialAttack: data.stats[3].base_stat,
+  specialDefense: data.stats[4].base_stat,
+  speed: data.stats[5].base_stat
+
+}
+
+// console.log(stats)
+info.stats = stats
+// console.log(info)
 
 // 10. Dentro de stats convierte el objeto en un array,
-// aplanalo y elimina los strings El resultado te tiene que quedar
+// aplanalo(quitar subarrays dentro de un array (pasar de un array bidimensional a unidimensional)) y elimina los strings El resultado te tiene que quedar
 // los valores numericos de las stats
+
+// console.log(Object.entries(info.stats))
+
+// para aplanar usamos el flat()
+info.stats = Object.entries(info.stats).flat()
+const statsNumerical = []
+
+for (let i = 0; i < info.stats.length; i++) {
+  if (typeof info.stats[i] !== 'string') {
+    statsNumerical.push(info.stats[i])
+  }
+}
+
+console.log(statsNumerical)
+
+info.stats = statsNumerical
+
+console.log(info)
